69dcfd003f1989f2fabca9464c88164869887fc5
sintiavm <marin.sintiavallet@gmail.com>
2021-09-19 00:57:18 -0500

    date

---
+++ js/date.js
@@ -0,0 +1,30 @@
+const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Agugust",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const todaysdate = new Date();
+const dayName =  dayNames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const currentdate = dayname  + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+
+document.getElementById('currentdate').textContent = currentdate;
