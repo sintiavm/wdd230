dc9ad7728f0b708e5315877d55d8918d30c2bf1a
sintiavm <marin.sintiavallet@gmail.com>
2021-10-30 12:40:04 -0500

    gallery

--- Lesson7/JS/gallery-7.js
+++ Lesson7/JS/gallery-7.js
@@ -1,50 +1,33 @@
-$(document).ready(function(){
-    $('td').hover( function () {
-    
-     $(this).animate({opacity:'0'});
-       },
-    
-        function () {
-    
-        $(this).animate({opacity:'0'});
-        }); 
-    });
-    
-    //get all imgs with dataa -src atribute
-const imagesToLoad = document.querySelectorAll("{img}")
-let imagesToLoad = document.querySelectorAll('img]');
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: .5
+};
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('img'));
+  image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
-    image.removeAttribute('.img');
+    image.removeAttribute('data-src');
   };
 };
+
 imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
     loadImages(img);
   });
-  
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  } 
-  function toggleMenu()  {
-    console.log(document.getElementById("newNav").classList)
-    document.getElementById("newNav").classList.toggle("hide");
 }
-let updateLast = document.lastModified
-document.getElementById('lastUpdate').innerHTML = updateLast
-let fullYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = fullYear;
